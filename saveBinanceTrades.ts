4246dadb64abbdef4d14ff8c895f211b7029a81c
Matlinski <50202894+matlinski@users.noreply.github.com>
2022-01-09 22:17:11 +0100

    looping through db result now

--- saveBinanceTrades.ts
+++ saveBinanceTrades.ts
@@ -1,6 +1,6 @@
-import { sleep } from "./deps.ts";
-import { binance } from './api/binance.ts';
+import { binance, autoRetry } from './api/binance.ts';
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { scrambleArray } from "./utils.ts";
 const binanceDB = new DB("db/binance.db");
 
 binanceDB.query(`
@@ -27,81 +27,66 @@ binanceDB.query(`
     UNIQUE(orderId)
   )
 `);
-const exchangeInfoResponse = await binance.exchangeInfo();
-if(exchangeInfoResponse.status !== 200) {
-  console.error(exchangeInfoResponse.statusText);
-  Deno.exit(1);
-}
-const exchangeInfo = exchangeInfoResponse.data;
-const allSymbols = exchangeInfo.symbols
-.map(
-    (symbolObject: any) => symbolObject.symbol
-)
-.map((value: any) => ({ value, sort: Math.random() }))
-.sort((a: any, b: any) => a.sort - b.sort)
-.map(({ value }: any) => value);
-for ( const symbol of allSymbols) {
-    try {
-      const allOrdersRequest = binance.allOrders(symbol);
-      allOrdersRequest.catch(console.error);
-      console.log(`trying requesting data for ${symbol}`);
-      const allOrdersResponse = await allOrdersRequest;
-      const allOrders = allOrdersResponse.data;
-      const filledOrders = allOrders.filter((order: any) => order.status === 'FILLED');
-      if(!filledOrders.length) continue;
-      for (const filledOrder of filledOrders) {
-        console.log(filledOrder);
-        binanceDB.query(`INSERT OR IGNORE INTO trade (
-          symbol,
-          orderId,
-          orderListId,
-          clientOrderId,
-          price,
-          origQty,
-          executedQty,
-          cummulativeQuoteQty,
-          status,
-          timeInForce,
-          type,
-          side,
-          stopPrice,
-          icebergQty,
-          time,
-          updateTime,
-          isWorking,
-          origQuoteOrderQty
-        ) VALUES (
-          :symbol,
-          :orderId,
-          :orderListId,
-          :clientOrderId,
-          :price,
-          :origQty,
-          :executedQty,
-          :cummulativeQuoteQty,
-          :status,
-          :timeInForce,
-          :type,
-          :side,
-          :stopPrice,
-          :icebergQty,
-          :time,
-          :updateTime,
-          :isWorking,
-          :origQuoteOrderQty
-        )`, filledOrder);
-      }
-    } catch (error) {
-      const timeout = Number(error.response.headers.get('retry-after'));
-      if(timeout > 0) {
-        console.log(`waiting ${timeout} seconds`);
-        await sleep(timeout);
-        allSymbols.unshift(symbol);
-        console.log(`adding ${symbol} back to the list`);
-      } else  {
-        console.error(error.response.data.msg);
-        break;
-      }
-    }
+const pairs = binanceDB.query('SELECT symbol FROM pair');
+const allSymbols = pairs.map(
+  (pair: any) => pair[0]
+  )
+  console.log(allSymbols);
+const allSymbolsScrambled = scrambleArray(allSymbols);
+
+for ( const symbol of allSymbolsScrambled) {
+  console.log(`trying requesting data for ${symbol}`);
+  const allOrdersResponse = await autoRetry(()=>binance.allOrders(symbol));
+  if(!allOrdersResponse) break;
+  if( allOrdersResponse === true){
+    allSymbolsScrambled.unshift(symbol);
+    console.log(`adding ${symbol} back to the list`);
+    continue;
+  }
+  const allOrders = allOrdersResponse.data;
+  const filledOrders = allOrders.filter((order: any) => order.status === 'FILLED');
+  if(!filledOrders.length) continue;
+  for (const filledOrder of filledOrders) {
+    console.log(filledOrder);
+    binanceDB.query(`INSERT OR IGNORE INTO trade (
+      symbol,
+      orderId,
+      orderListId,
+      clientOrderId,
+      price,
+      origQty,
+      executedQty,
+      cummulativeQuoteQty,
+      status,
+      timeInForce,
+      type,
+      side,
+      stopPrice,
+      icebergQty,
+      time,
+      updateTime,
+      isWorking,
+      origQuoteOrderQty
+    ) VALUES (
+      :symbol,
+      :orderId,
+      :orderListId,
+      :clientOrderId,
+      :price,
+      :origQty,
+      :executedQty,
+      :cummulativeQuoteQty,
+      :status,
+      :timeInForce,
+      :type,
+      :side,
+      :stopPrice,
+      :icebergQty,
+      :time,
+      :updateTime,
+      :isWorking,
+      :origQuoteOrderQty
+    )`, filledOrder);
+  }
 }
 binanceDB.close();
