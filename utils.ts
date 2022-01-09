57b35db94b05bf01b34f059b3350573bd3e88f7b
Matlinski <50202894+matlinski@users.noreply.github.com>
2022-01-09 22:18:13 +0100

    scrambleArray added

---
+++ utils.ts
@@ -0,0 +1,4 @@
+export const scrambleArray = (a: any[]) =>
+    a.map((value: any) => ({ value, sort: Math.random() }))
+    .sort((a: any, b: any) => a.sort - b.sort)
+    .map(({ value }: any) => value);
