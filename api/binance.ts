304f22013e6d50f8060318f6725047537d1d8edb
Matlinski <50202894+matlinski@users.noreply.github.com>
2022-01-09 22:18:05 +0100

    autoRetry added needs reworking

--- api/binance.ts
+++ api/binance.ts
@@ -1,4 +1,38 @@
+import { sleep } from '../deps.ts';
 import { Spot } from 'https://esm.sh/@binance/connector';
 import { binanceAPIaccess } from './credentials.ts';
 const { apiKey, secretKey } = binanceAPIaccess;
 export const binance = new Spot(apiKey, secretKey);
+export const autoRetry = async (f: CallableFunction) => {
+    try {
+        return await f();
+    }   catch (e){
+        console.log(e);
+        const timeout = Number(e.response.headers.get('retry-after'));
+        if(timeout <= 0) {
+          console.error(e.response.data.msg);
+          return false;
+        } 
+        console.log(`waiting ${timeout} seconds`);
+        await sleep(timeout);
+        return true;
+    }
+}
+
+/* VERSION TO IMPLEMENT
+export const autoRetry = <F extends CallableFunction>(f: F) => async(...args: Parameters<F>) => {
+    try {
+      return await f(...args);
+    } catch (err) {
+        console.log(err);
+        const timeout = Number(err.response.headers.get('retry-after'));
+        if(timeout <= 0) {
+          console.error(err.response.data.msg);
+          return false;
+        } 
+        console.log(`waiting ${timeout} seconds`);
+        await sleep(timeout);
+        return true;
+    }
+  };
+*/
