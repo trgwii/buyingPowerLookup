9c383c92f86740a9416aff7175a498e54b2ae0ae
Matlinski <50202894+matlinski@users.noreply.github.com>
2022-01-15 21:09:41 +0100

    added deps

--- deps.ts
+++ deps.ts
@@ -2,3 +2,7 @@ export { hmac } from "https://deno.land/x/hmac@v2.0.1/mod.ts";
 export * as z from "https://deno.land/x/zod@v3.11.6/mod.ts";
 export { existsSync } from "https://deno.land/std/fs/mod.ts";
 export { sleep } from "https://deno.land/x/sleep/mod.ts";
+export { DB } from "https://deno.land/x/sqlite/mod.ts";
+export { calculateFIFOCapitalGains } from "https://esm.sh/fifo-capital-gains-js";
+export type { Operation } from "https://esm.sh/fifo-capital-gains-js";
+export { Spot } from "https://esm.sh/@binance/connector";
