67843d556ac13b0c189437badc7a4c6bed24d978
Matlinski <50202894+matlinski@users.noreply.github.com>
2022-02-14 21:06:05 +0100

    fees added to the calculation

---
+++ BinanceCommission2Transaction.ts
@@ -0,0 +1,64 @@
+import { DB } from "./deps.ts";
+
+const binanceDB = new DB("db/binance.db");
+
+binanceDB.query(`
+  CREATE TABLE IF NOT EXISTS \`transaction\` (
+    transactionID INTEGER PRIMARY KEY AUTOINCREMENT,
+    type                  CHARACTER(20),
+    refId                 INTEGER,
+    asset                 CHARACTER(20),
+    side                  BOOLEAN,
+    amount                FLOAT,
+    price                 FLOAT,
+    timestamp             INTEGER,
+    UNIQUE(type, refId, side)
+  )
+`);
+const allCommissions = binanceDB.query(
+  "SELECT commissionID, symbol AS 's', qty AS 'q1', quoteQty AS 'q2', `time` as 'date', isBuyer AS 'i', commission, commissionAsset FROM commission",
+);
+for (const tradeData of allCommissions) {
+  const [
+    commissionID,
+    s,
+    q1,
+    q2,
+    date,
+    i,
+    commission,
+    commissionAsset,
+  ] = tradeData;
+  const dateUTC = new Date(Number(date));
+  const createTime = dateUTC.getTime() + 1;
+  binanceDB.query(
+    `INSERT OR IGNORE INTO \`transaction\` (
+          type,
+          refId,
+          asset,
+          side,
+          amount,
+          price,
+          timestamp
+        ) VALUES (
+          ?,
+          ?,
+          ?,
+          ?,
+          ?,
+          ?,
+          ?
+        )`,
+    [
+      "commission",
+      Number(commissionID),
+      String(commissionAsset),
+      "OUT",
+      Number(commission),
+      0,
+      Number(createTime),
+    ],
+  );
+  console.log("adding commission for asset:", commissionAsset, commission);
+}
+binanceDB.close();
---
+++ saveBinanceCommissions.ts
@@ -0,0 +1,84 @@
+import { autoRetry, binance } from "./api/binance.ts";
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { scrambleArray } from "./utils.ts";
+const binanceDB = new DB("db/binance.db");
+
+binanceDB.query(`
+  CREATE TABLE IF NOT EXISTS commission (
+    commissionID INTEGER PRIMARY KEY AUTOINCREMENT,
+    symbol                CHARACTER(20),
+    id                    INTEGER,
+    orderId               INTEGER,
+    orderListId           INTEGER,
+    clientOrderId         VARCHAR(50),
+    price                 FLOAT,
+    qty                   FLOAT,
+    quoteQty              FLOAT,
+    commission            FLOAT,
+    commissionAsset       CHARACTER(20),
+    time                  INTEGER,
+    isBuyer               BOOLEAN,
+    isMaker               BOOLEAN,
+    isBestMatch           BOOLEAN,
+    UNIQUE(orderId)
+  )
+`);
+const pairs = binanceDB.query("SELECT symbol FROM pair");
+const allSymbols = pairs.map(
+  (pair: any) => pair[0],
+);
+console.log(allSymbols);
+const allSymbolsScrambled = scrambleArray(allSymbols);
+
+for (const symbol of allSymbolsScrambled) {
+  console.log(`trying requesting data for ${symbol}`);
+  const allCommissionsResponse = await autoRetry(() =>
+    binance.myTrades(symbol)
+  );
+  if (!allCommissionsResponse) break;
+  const allCommissions = allCommissionsResponse.data;
+  if (!allCommissions.length) continue;
+  for (const commission of allCommissions) {
+    if (
+      Number(commission.commission) <= 0
+    ) {
+      continue;
+    }
+    console.log(commission);
+    binanceDB.query(
+      `INSERT OR IGNORE INTO commission (
+        symbol,
+        id,
+        orderId,
+        orderListId,
+        clientOrderId,
+        price,
+        qty,
+        quoteQty,
+        commission,
+        commissionAsset,
+        time,
+        isBuyer,
+        isMaker,
+        isBestMatch
+    ) VALUES (
+      :symbol,
+      :id,
+      :orderId,
+      :orderListId,
+      :clientOrderId,
+      :price,
+      :qty,
+      :quoteQty,
+      :commission,
+      :commissionAsset,
+      :time,
+      :isBuyer,
+      :isMaker,
+      :isBestMatch
+    )`,
+      commission,
+    );
+  }
+}
+binanceDB.close();
