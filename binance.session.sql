3b15642362fa7036ff5200a63e4235f3d40451bd
Matlinski <50202894+matlinski@users.noreply.github.com>
2022-02-23 10:11:09 +0100

    updating test file

--- binance.session.sql
+++ binance.session.sql
@@ -1,21 +1,2 @@
-SELECT
-datetime(time/1000, 'unixepoch') AS 'datetime',
-pair.quoteAsset,
-CASE WHEN side = 'BUY'
-THEN cummulativeQuoteQty
-ELSE executedQty
-END AS 'amount',
-CASE WHEN side LIKE 'BUY'
-THEN ROUND(
-fiatPrice, 2
-)
-ELSE ROUND(
-fiatPrice, 2
-)
-END AS "price"
-FROM trade
-JOIN tradeFiatPrice
-ON trade.tradeID = tradeFiatPrice.tradeID
-JOIN pair
-ON trade.symbol = pair.symbol
-WHERE datetime LIKE '2021%'
+SELECT * FROM `transaction` WHERE `type` = "trade"
+-- DELETE FROM `transaction` WHERE `type` = 'trade'
--- test.ts
+++ test.ts
@@ -1,42 +1,76 @@
-import { calculateFIFOCapitalGains, DB } from "./deps.ts";
+import { aggregateByYear, calculateFIFOCapitalGains, DB } from "./deps.ts";
 import type { Operation } from "./deps.ts";
 
 const binanceDB = new DB("db/binance.db");
 
-const allTrades = binanceDB.query<
-  [number, string, number, string, "BUY" | "SELL"]
->(
-  `SELECT
-    CASE WHEN side = 'BUY'
-    THEN cummulativeQuoteQty
-    ELSE executedQty
-    END,
-    datetime(time/1000, 'unixepoch') AS 'datetime',
-    CASE WHEN side LIKE 'BUY'
-    THEN fiatPrice
-    ELSE fiatPrice
-    END,
-    CASE WHEN side = 'BUY'
-    THEN pair.baseAsset
-    ELSE pair.quoteAsset
-    END,
-    side
-    FROM trade
-    JOIN tradeFiatPrice
-    ON trade.tradeID = tradeFiatPrice.tradeID
-    JOIN pair
-    ON trade.symbol = pair.symbol
-    WHERE datetime LIKE '2021%'`,
+const allTransaction = binanceDB.query<
+  [number, number, number, string, "IN" | "OUT"]
+>( //1640991600000 <= until 2021
+  `SELECT amount, timestamp, price, asset, side FROM \`transaction\` WHERE timestamp < 1640991600000 AND asset != 'EUR' ORDER BY timestamp ASC`,
 );
-const operationHistory = allTrades.map(
-  (operation): Operation => ({
+const operationHistory = allTransaction.map(
+  (operation: any): Operation => ({
     amount: operation[0],
     date: new Date(operation[1]),
     price: operation[2],
     symbol: operation[3],
-    type: operation[4],
+    type: operation[4] === "IN" ? "BUY" : "SELL",
   }),
 );
+const assetAmountCheck = {};
+/*
+const missingAssets = {};
+*/
+let missingCosts = 0;
+for (const operationEntry of operationHistory) {
+  if (!(operationEntry.symbol in assetAmountCheck)) {
+    assetAmountCheck[operationEntry.symbol] = 0;
+  }
+  /*
+  if (!(operationEntry.symbol in missingAssets)) {
+    missingAssets[operationEntry.symbol] = 0;
+  }
+  */
+  assetAmountCheck[operationEntry.symbol] += operationEntry.type === "BUY"
+    ? operationEntry.amount
+    : -operationEntry.amount;
+  /*
+  console.log(
+    operationEntry.symbol,
+    operationEntry.amount,
+    operationEntry.date,
+    operationEntry.type,
+    assetAmountCheck["ALGO"],
+  );
+  */
+  if (
+    operationEntry.type === "SELL" &&
+    assetAmountCheck[operationEntry.symbol] < 0
+  ) {
+    /*
+    missingAssets[operationEntry.symbol] += 1.00000000001 *
+      Math.abs(assetAmountCheck[operationEntry.symbol]);
+      */
+    missingCosts += operationEntry.price * 1.00000000001 *
+      Math.abs(assetAmountCheck[operationEntry.symbol]);
+    operationHistory.push({
+      amount: 1.00000000001 *
+        Math.abs(assetAmountCheck[operationEntry.symbol]),
+      date: new Date(
+        operationEntry.date.getTime() - 1,
+      ),
+      price: 0,
+      symbol: operationEntry.symbol,
+      type: "BUY",
+    });
+    assetAmountCheck[operationEntry.symbol] = 0;
+  }
+}
+
 const capitalGains = calculateFIFOCapitalGains(operationHistory);
 
-console.log(capitalGains);
+const gainsByYear = aggregateByYear(capitalGains);
+
+// console.log(missingAssets);
+console.log(gainsByYear);
+console.log(missingCosts);
