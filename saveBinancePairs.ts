078aba6cbb8273316fcc1fbc5796a50544ec877a
Matlinski <50202894+matlinski@users.noreply.github.com>
2022-02-25 09:29:38 +0100

    pair request using autoretry

--- saveBinancePairs.ts
+++ saveBinancePairs.ts
@@ -1,12 +1,12 @@
 import { BinancePair } from "./api/api2db.ts";
-import { binance } from "./api/binance.ts";
+import { autoRetry, binance } from "./api/binance.ts";
 import { DB } from "./deps.ts";
 const binanceDB = new DB("db/binance.db");
 const binancePair = BinancePair(binanceDB);
 binancePair.init();
-const exchangeInfoResponse = await binance.exchangeInfo();
-if (exchangeInfoResponse.status !== 200) {
-  console.error(exchangeInfoResponse.statusText);
+const exchangeInfoResponse = await autoRetry(() => binance.exchangeInfo());
+if (!exchangeInfoResponse || !exchangeInfoResponse.data) {
+  console.error(exchangeInfoResponse);
   Deno.exit(1);
 }
 const exchangeInfo = exchangeInfoResponse.data;
