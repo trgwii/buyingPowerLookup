f4d89372adc1dece763b3f4b563fb73bb168d701
Mateusz Matlinski <mateusz@matlinski.com>
2022-11-27 20:59:42 +0100

    updated deps

--- polyfills/axios.ts
+++ polyfills/axios.ts
@@ -1,3 +1,3 @@
-import axios from "https://deno.land/x/axiod@0.24/mod.ts";
+import axios from "https://deno.land/x/axiod@0.26.2/mod.ts";
 export default axios;
 export const create = axios.create.bind(axios);
