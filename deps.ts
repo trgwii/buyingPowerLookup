321aed9fd6ca79e8f0be7da684004d10378af201
Mateusz Matlinski <mateusz@matlinski.com>
2022-11-27 20:59:20 +0100

    updated deps

--- deps.ts
+++ deps.ts
@@ -1,19 +1,19 @@
 export { hmac } from "https://deno.land/x/hmac@v2.0.1/mod.ts";
 export * as z from "https://deno.land/x/zod@v3.11.6/mod.ts";
-export { existsSync } from "https://deno.land/std@0.126.0/fs/mod.ts";
+export { existsSync } from "https://deno.land/std@0.140.0/fs/mod.ts";
 export { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.2.1/mod.ts";
 export type { Row } from "https://deno.land/x/sqlite@v3.2.1/mod.ts";
 export { DB as apiDB } from "https://deno.land/x/sqlite@v2.4.1/mod.ts";
 export { Order, Query } from "https://deno.land/x/sql_builder@v1.9.0/mod.ts";
-import { Spot as _Spot } from "https://esm.sh/@binance/connector@1.7.0?dev";
+import { Spot as _Spot } from "https://esm.sh/@binance/connector@2.0.0-rc.4?dev";
 import type { SpotClass } from "./api/binance/Spot.d.ts";
 export const Spot: typeof SpotClass = _Spot;
 export { elements, renderHTML } from "https://deno.land/x/hyperactive/mod.ts";
 
 import {
   parse as parseCsv,
-} from "https://deno.land/std@0.126.0/encoding/csv.ts";
+} from "https://deno.land/std@0.140.0/encoding/csv.ts";
 
 import PQueue from "https://deno.land/x/p_queue@1.0.1/mod.ts";
 import type Task from "https://deno.land/x/p_queue@1.0.1/mod.ts";
